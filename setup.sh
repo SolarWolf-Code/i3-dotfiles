2eb659bbed3baf87d1714f314720707e8492fd55
SolarWolf <bradsterstrange0@gmail.com>
2023-09-09 18:15:38 -0600

    add needed base-devel

--- setup.sh
+++ setup.sh
@@ -9,10 +9,11 @@ cd i3-dotfiles
 
 cp -r .config ~/
 cp -r .icons ~/
-cp -r .local/share ~/.local
+cp -r .local/share ~/.local/share
 
 # adding ble.sh (autocomplete in bash)
 cd 
+sudo pacman -S base-devel
 git clone --recursive --depth 1 --shallow-submodules https://github.com/akinomyoga/ble.sh.git
 make -C ble.sh install PREFIX=~/.local
 echo 'source ~/.local/share/blesh/ble.sh' >> ~/.bashrc
